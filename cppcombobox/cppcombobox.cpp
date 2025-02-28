289d6a62483d96ceda9ebd2ef8be81fd06965023
oleh-tft <tiftelalbion@gmail.com>
2025-02-28 16:06:47 +0200

    Added editbox text

--- cppcombobox/cppcombobox.cpp
+++ cppcombobox/cppcombobox.cpp
@@ -4,12 +4,14 @@
 
 BOOL CALLBACK DlgProc(HWND, UINT, WPARAM, LPARAM);
 
-HWND hGroup, hStudents, hResult;
-
 int WINAPI _tWinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPTSTR lpszCmdLine, int nCmdShow)
 {
     return DialogBox(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), NULL, (DLGPROC)DlgProc);
 }
+
+HWND hGroup, hStudents, hResult, hRadio1, hRadio2, hRadio3;
+int groupId, studentId = -1;
+TCHAR buf[10];
 
 TCHAR groups[2][10] = {
     { TEXT("สอ-ฯ-231") },
@@ -35,10 +37,19 @@ double marks[2][2][3] = {
     },
 
     { 
-        { {8.9}, {10.9}, {16.68} },
+        { {8.9}, {10.9}, {11.6} },
         { {9.9}, {9.9}, {9.9} }
     }
 };
+
+void ResetControls(HWND& hWnd)
+{
+    SendDlgItemMessage(hWnd, IDC_RADIO1, BM_SETCHECK, BST_UNCHECKED, 0);
+    SendDlgItemMessage(hWnd, IDC_RADIO2, BM_SETCHECK, BST_UNCHECKED, 0);
+    SendDlgItemMessage(hWnd, IDC_RADIO3, BM_SETCHECK, BST_UNCHECKED, 0);
+    SendMessage(hResult, WM_SETTEXT, 0, NULL);
+    studentId = -1;
+}
 
 BOOL CALLBACK DlgProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
@@ -48,34 +59,58 @@ BOOL CALLBACK DlgProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
         hGroup = GetDlgItem(hWnd, IDC_COMBO1);
         hStudents = GetDlgItem(hWnd, IDC_LIST1);
         hResult = GetDlgItem(hWnd, IDC_EDIT1);
+        hRadio1 = GetDlgItem(hWnd, IDC_RADIO1);
+        hRadio2 = GetDlgItem(hWnd, IDC_RADIO2);
+        hRadio3 = GetDlgItem(hWnd, IDC_RADIO3);
 
         for (int i = 0; i < 2; i++)
         {
             SendMessage(hGroup, CB_ADDSTRING, i, LPARAM(groups[i]));
-
-            for (int j = 0; j < 2; j++)
-            {
-
-            }
         }
 
         return TRUE;
     case WM_COMMAND:
-
-
-        if (LOWORD(wParam) == IDC_LIST1 && HIWORD(wParam) == CBN_SELCHANGE)
+        if (LOWORD(wParam) == IDC_COMBO1 && HIWORD(wParam) == CBN_SELCHANGE)
         {
+            ResetControls(hWnd);
             SendMessage(hStudents, LB_RESETCONTENT, 0, 0);
+
             int index = SendMessage(hGroup, CB_GETCURSEL, 0, 0);
-            switch (index)
+            for (int i = 0; i < 2; i++)
             {
-            case 0:
-                for (int i = 0; i < 2; i++)
-                {
-                    SendMessage(hStudents, LB_SETITEMDATA, index, (LPARAM)students[i]);
-                }
-                break;
+                SendMessage(hStudents, LB_ADDSTRING, i, (LPARAM)students[index][i]);
             }
+            groupId = index;
+        }
+
+        if (LOWORD(wParam) == IDC_LIST1 && HIWORD(wParam) == LBN_SELCHANGE)
+        {
+            ResetControls(hWnd);
+
+            int index = SendMessage(hStudents, LB_GETCURSEL, 0, 0);
+            studentId = index;
+        }
+
+        for (int i = 0; i < 3; i++)
+        {
+            if (wParam == IDC_RADIO1 + i)
+            {
+                swprintf(buf, 10, TEXT("%.1f"), marks[groupId][studentId][i]);
+                SendMessage(hResult, WM_SETTEXT, 0, LPARAM(buf));
+            }
+        }
+
+        if (studentId != -1)
+        {
+            EnableWindow(hRadio1, TRUE);
+            EnableWindow(hRadio2, TRUE);
+            EnableWindow(hRadio3, TRUE);
+        }
+        else
+        {
+            EnableWindow(hRadio1, FALSE);
+            EnableWindow(hRadio2, FALSE);
+            EnableWindow(hRadio3, FALSE);
         }
 
         if (wParam == IDC_BUTTON1)
